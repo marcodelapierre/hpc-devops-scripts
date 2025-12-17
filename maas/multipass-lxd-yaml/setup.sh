307cc6077dee8bdbe60d9bf161ff89512b438766
Dr Marco De La Pierre <marco.delapierre@gmail.com>
2025-12-17 15:30:12 +0800

    minor updates
    Signed-off-by: Dr Marco De La Pierre <marco.delapierre@gmail.com>

--- maas/multipass-lxd-yaml/setup.sh
+++ maas/multipass-lxd-yaml/setup.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-multipass launch --name maas -c4 -m8GB -d32GB --timeout 3600 --cloud-init maas.yaml
+multipass launch --name maas -c4 -m8GB -d32GB --timeout 3600 --cloud-init maas.yaml 24.04
+
+# in your web browser: http://<MAAS_IP>:5240/MAAS
