00a6986f61c85110b2224ec93295c8fc57fb6b4d
Dr Marco De La Pierre <marco.delapierre@gmail.com>
2026-01-07 09:45:59 +0800

    ww: updating Vagrantfile
    Signed-off-by: Dr Marco De La Pierre <marco.delapierre@gmail.com>

---
+++ warewulf/vagrant/create_workers.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+CPUS=2
+MEM=4096
+DISK=10
+NETWORK="maas-net"
+
+for i in 1 ; do
+  NID=$(printf "%02g" $i)
+  VM_NAME="node${NID}"
+  MAC_ADDR="0e:00:00:00:00:${NID}"
+  
+  echo "Creating $VM_NAME..."
+  
+  virt-install \
+    --name "$VM_NAME" \
+    --vcpus $CPUS \
+    --memory $MEM \
+    --disk size=$DISK,bus=virtio \
+    --network network=$NETWORK,mac=$MAC_ADDR,model=virtio \
+    --pxe \
+    --boot network \
+    --virt-type kvm \
+    --os-variant ubuntu24.04 \
+    --noautoconsole
+    
+  echo "...Done creating $VM_NAME."
+done
