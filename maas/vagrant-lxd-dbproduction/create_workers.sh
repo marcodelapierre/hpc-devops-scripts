e1b3815d39ca0bc4655ae5a0ec41edd109dd968a
Dr Marco De La Pierre <marco.delapierre@gmail.com>
2026-01-20 22:01:59 +0800

    maas vagrant lxd: using maas project
    Signed-off-by: Dr Marco De La Pierre <marco.delapierre@gmail.com>

--- maas/vagrant-lxd-dbproduction/create_workers.sh
+++ maas/vagrant-lxd-dbproduction/create_workers.sh
@@ -4,7 +4,15 @@
 #lxc network set lxdbr0 ipv4.dhcp=false
 #lxc network set lxdbr0 ipv6.dhcp=false
 
-lxc init node01 --empty --vm --network lxdbr0 -c limits.cpu=1 -c limits.memory=4GiB -d root,size=8GiB
-lxc start node01
+lxc init \
+  node01 \
+  --empty \
+  --vm \
+  --network lxdbr0 \
+  -c limits.cpu=1 \
+  -c limits.memory=4GiB \
+  -d root,size=8GiB \
+  --project maas
+#lxc start node01 --project maas
 
-#lxc delete node01
+#lxc delete node01 --project maas
