3b0a909921f44954b0c975906a7fb69d0eaf40ba
Dr Marco De La Pierre <marco.delapierre@gmail.com>
2026-01-22 00:33:41 +0800

    kubernetes, small update to init-microk8s.sh
    Signed-off-by: Dr Marco De La Pierre <marco.delapierre@gmail.com>

--- kubernetes/init-microk8s.sh
+++ kubernetes/init-microk8s.sh
@@ -4,26 +4,28 @@
 
 vmuser="ubuntu"
 
-# Install and configure Kubernetes
+# Install microk8s
 snap install microk8s --classic --channel=1.35
-
-adduser $vmuser microk8s
-mkdir -p /home/$vmuser/.kube
-chown -R $vmuser:$vmuser /home/$vmuser/.kube
-chmod 700 /home/$vmuser/.kube
-#newgrp microk8s
 
 microk8s status --wait-ready
 for plugin in dns dashboard hostpath-storage ingress ; do
     microk8s enable $plugin
 done
 
-# Install and configure kubectl
+# Install kubectl
 #alias kubectl='microk8s kubectl'
 #kubectl_ver="$(curl -L -s https://dl.k8s.io/release/stable.txt)"
 kubectl_ver="v1.35.0"
 curl -L "https://dl.k8s.io/release/${kubectl_ver}/bin/linux/amd64/kubectl" -o /usr/local/bin/kubectl
 chmod +x /usr/local/bin/kubectl
+
+# Configure $vmuser access to k8s
+adduser $vmuser microk8s
+#newgrp microk8s
+
+mkdir -p /home/$vmuser/.kube
+chown -R $vmuser:$vmuser /home/$vmuser/.kube
+chmod 700 /home/$vmuser/.kube
 
 microk8s config >/root/.kube/config
 microk8s config >/home/$vmuser/.kube/config
